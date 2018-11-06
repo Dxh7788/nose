3f81d136c7fb2b94f0168e2aea5865d1d1a77d84
dongxiaohong <dongxiaohong@daokoudai.com>
2018-11-06 19:54:07 +0800

    vue.html

--- src/main/webapp/pages/vue/template/App.vue
+++ src/main/webapp/pages/vue/template/App.vue
@@ -1,13 +1,55 @@
 <template>
-    $END$
+    <!--定义的是html模板-->
+    <p>{{greeting}},World!!!</p>
 </template>
 
 <script>
+    import 'jquery';
+    /*相当于new Vue*/
     export default {
-        name: "App"
+        name: "App",
+        data () {
+            return {
+                greeting:"Hello"
+            }
+        },
+        beforeCreate(){
+            this.before_create();
+        },
+        create(){
+            this.v_create();
+        },
+        beforeMount(){
+            this.before_mount();
+        },
+        mounted(){
+            this.v_mounted();
+        },
+        beforeUpdate(){
+            this.beforeUpdate();
+        },
+        /*方法定义区域*/
+        methods:{
+            /**/
+            before_create(){
+                console.log(document);
+            },
+            v_create(){
+                console.log(document);
+            },
+            before_mount(){
+                console.log(document);
+            },
+            v_mounted(){
+                console.log(document);
+            },
+            before_update(){
+                console.log(document);
+            }
+        }
     }
 </script>
 
 <style scoped>
-
+    @import "../../../style/vue/main.css";
 </style>
