3e6ce3d0b3cbd34b89317208721bf225f3ca3a81
dongxiaohong <dongxiaohong@daokoudai.com>
2018-11-07 19:07:05 +0800

    modify config

--- src/main/webapp/webpack/webpack.config.js
+++ src/main/webapp/webpack/webpack.config.js
@@ -1,5 +1,6 @@
 let webpack = require('webpack');
-module.exports = {
+//导出多个配置对象
+module.exports = [{
     //__dirname”是node.js中的一个全局变量，它指向当前执行脚本所在的目录
     entry: {
         bundles1:__dirname+"/app/main.js",
@@ -18,5 +19,15 @@ module.exports = {
     plugins:[
         new webpack.BannerPlugin("版权所有,翻版必究!!!")
     ],
+},{
+    entry:{
+        bundles3:__dirname+"/app/main.js",
+        bundles4:__dirname+"/app/main.js"
+    },
+    output:{
+        path:__dirname+"/public",
+        filename:"[name].[id].[hash].[chunkhash].bundle.js"
+    }
 }
+]
 //下面是大名鼎鼎的Loaders,比如babel-loader、css-loader、style-loader、less-loader、Sass-loader、Stylus Loader
